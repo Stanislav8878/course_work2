0fcf55cc1b9cf8355a5b13616c9b4d5e77d1753a
Станислав Степаненко <ronin999@list.ru>
2025-08-24 19:57:37 +0300

    Изменения:
    В AbstractAPI метод _connect_to_api теперь абстрактный
    
    В HeadHunterAPI:
    
    url сделан приватным (__url)
    
    Реализация _connect_to_api перенесена из абстрактного класса
    
    Добавлен импорт requests

--- src/abstract_api.py
+++ src/abstract_api.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-import requests
 from typing import Dict, Any
 
 
@@ -10,9 +9,7 @@ class AbstractAPI(ABC):
     def get_vacancies(self, search_query: str) -> list[Dict[str, Any]]:
         pass
 
+    @abstractmethod
     def _connect_to_api(self, url: str, params: Dict[str, Any]) -> Dict[str, Any]:
         """Приватный метод для подключения к API."""
-        response = requests.get(url, params=params)
-        if response.status_code != 200:
-            raise Exception(f"Ошибка подключения к API: {response.status_code}")
-        return response.json()
+        pass
--- src/hh_api.py
+++ src/hh_api.py
@@ -1,15 +1,23 @@
 from src.abstract_api import AbstractAPI
 from typing import Dict, Any, List
+import requests
 
 
 class HeadHunterAPI(AbstractAPI):
     """Класс для работы с API HeadHunter."""
 
     def __init__(self) -> None:
-        self.url = "https://api.hh.ru/vacancies"
+        self.__url = "https://api.hh.ru/vacancies"
         self.headers = {"User-Agent": "HH-User-Agent"}
         self.params: Dict[str, Any] = {"text": "", "page": 0, "per_page": 100}
         self.vacancies: List[Dict[str, Any]] = []
+
+    def _connect_to_api(self, url: str, params: Dict[str, Any]) -> Dict[str, Any]:
+        """Приватный метод для подключения к API."""
+        response = requests.get(url, params=params, headers=self.headers)
+        if response.status_code != 200:
+            raise Exception(f"Ошибка подключения к API: {response.status_code}")
+        return response.json()
 
     def get_vacancies(self, search_query: str) -> List[Dict[str, Any]]:
         """Получение вакансий с hh.ru по поисковому запросу."""
@@ -18,7 +26,7 @@ class HeadHunterAPI(AbstractAPI):
         self.vacancies.clear()
 
         while self.params["page"] < 20:  # Ограничение в 2000 вакансий
-            data = self._connect_to_api(self.url, self.params)
+            data = self._connect_to_api(self.__url, self.params)
             self.vacancies.extend(data["items"])
 
             # Преобразуем page в int для сравнения
